24c394e7ebe8971750e5026d842ed437bbc2d59b
edozzz06 <edoardovolley06@gmail.com>
2026-02-23 20:22:01 +0100

    Refactor db connection for production

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
+const isPruduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: isPruduction 
+    ? { rejectUnauthorized: false} 
+    : false
 });
 
 module.exports = pool;
@@ -14,4 +15,11 @@ module.exports = pool;
 /*
 This file allows connecton with the database through a pool that keeps connection active so that 
 the app can perform multiple queries after a single handshake
+
+FOR DEVELOPMENT it used to be
+    user: process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 */
